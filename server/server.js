1197948b0c19e0bc4b6933d9fdc7337b7f633d2c
Shreyas Acharya <97395321+enkrypton214@users.noreply.github.com>
2026-03-14 13:12:16 +0545

    API Endpoint and basic server
    Connected an API endpoint and used thunderclient to check it

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const http = require("http"); //http module
+const { json } = require("stream/consumers");
+
+const port = 3000; //assigning port number
+const server = http.createServer((req, res) => {
+	//Home
+	if (req.url === "/" && req.method === "GET") {
+		res.writeHead(200, { "content-type": "text/plain" });
+		res.end("AI Detective Server Running");
+		return;
+	}
+
+	// API EndPoint
+	if (req.url === "/ask" && req.method === "POST") {
+		let body = "";
+		//Recieve data
+		req.on("data", (chunk) => {
+			body += chunk.toString();
+		});
+		req.on("end", () => {
+			const data = JSON.parse(body);
+			const question = data.question;
+
+			const response = { answer: "You asked: " + question };
+
+			res.writeHead(200, { "content-type": "application/json" });
+			res.end(JSON.stringify(response));
+		});
+		return;
+	}
+
+	//Error Catch
+	res.writeHead(404, { "content-type": "text/plain" });
+	res.end("Route not found");
+}); // create server and check if it exists or not
+//model = client(browser)=> Req => server => Res => client(Browser)
+//process node starts server.js => server.listen starts listening => browser req => create server Runs => check URL => send Response
+
+server.listen(port, () => {
+	console.log(`Server Running on port ${port}`);
+}); //
